a9a46973a3c7d1dd0e058c6741a8cdde4a4fd8d1
Jeffrey Hein <jp@jphein.com>
2019-08-26 14:59:51 -0700

    Update installduo.sh

--- installduo.sh
+++ installduo.sh
@@ -8,9 +8,9 @@ wget http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24
 wget http://www.easyuo.com/forum/download/file.php?id=6826&sid=40837823159df0477bd9d831f1ea9b4b
 #NOTE: EasyUO is probably considered by EA an UNAUTHORIZED 3rd party program. Use only on servers that allow it.  
 #node.js
-wget [[https://nodejs.org/dist/v10.16.3/node-v10.16.3-x86.msi]]
+wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-x86.msi
 #nircmd.exe (for taking screenshots from the cmd line) 
-wget [[https://www.nirsoft.net/utils/nircmd.zip]]
+wget https://www.nirsoft.net/utils/nircmd.zip
 #curl 
 sudo apt get install curl
 # * jq
