db1324555e3bd94ccc22a2c750d67b79a7c6bd5f
Jeffrey Hein <jp@jphein.com>
2019-08-26 15:20:55 -0700

    Update installduo.sh

--- installduo.sh
+++ installduo.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 #Run the following bash commands:
-cd /mnt/c/Users/$USER/Desktop/duo
+#Update
 sudo apt update --yes && sudo apt upgrade --yes
+#Git if not already installed
+sudo apt install --yes git
+cd /mnt/c/Users/$USER/Desktop/              #Change to the Desktop
+git clone https://github.com/jphein/duo.git #Git clone the duo repository pt install --yes git
+cd /mnt/c/Users/$USER/Desktop/duo
+
 #Ultima Online Classic Client 
 wget http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24_0.exe
+#run install
+
 #EasyUO 
 wget http://www.easyuo.com/forum/download/file.php?id=6826&sid=40837823159df0477bd9d831f1ea9b4b
+#Unzip and move to duo folder
+
 #NOTE: EasyUO is probably considered by EA an UNAUTHORIZED 3rd party program. Use only on servers that allow it.  
 #node.js
 wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-x86.msi
+#run install
+
 #nircmd.exe (for taking screenshots from the cmd line) 
 wget https://www.nirsoft.net/utils/nircmd.zip
+#Unzip and move to duo folder
+
 #curl 
-sudo apt get install curl
+sudo apt install --yes curl jq
 # * jq
-sudo apt get install jq
+#sudo apt get install jq
+
 sudo chmod u+x ./easyuo2discord.sh
 npm install --save discord.js
