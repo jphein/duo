78e25a0f7949e60b85ce2bcdb31f0552e4e358a4
Jeffrey Hein <jp@jphein.com>
2019-08-26 15:38:18 -0700

    Update installduo.sh

--- installduo.sh
+++ installduo.sh
@@ -4,31 +4,34 @@
 sudo apt update --yes && sudo apt upgrade --yes
 #Git if not already installed
 sudo apt install --yes git
+#Unzip
+sudo apt install --yes unzip
+#curl 
+sudo apt install --yes curl
+# jq
+sudo apt install --yes jq
+#Install duo
 cd /mnt/c/Users/$USER/Desktop/              #Change to the Desktop
 git clone https://github.com/jphein/duo.git #Git clone the duo repository pt install --yes git
 cd /mnt/c/Users/$USER/Desktop/duo
-
 #Ultima Online Classic Client 
-wget http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24_0.exe
+curl -o uoc.exe http://web.cdn.eamythic.com/us/uo/installers/20120309/UOClassicSetup_7_0_24_0.exe
 #run install
-
+./uoc.exe
 #EasyUO 
-wget http://www.easyuo.com/forum/download/file.php?id=6826&sid=40837823159df0477bd9d831f1ea9b4b
+curl -o easyuo.zip http://www.easyuo.com/forum/download/file.php?id=6826
 #Unzip and move to duo folder
-
+unzip easyuo.zip
 #NOTE: EasyUO is probably considered by EA an UNAUTHORIZED 3rd party program. Use only on servers that allow it.  
 #node.js
-wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-x86.msi
+curl -o node.msi https://nodejs.org/dist/v10.16.3/node-v10.16.3-x86.msi
 #run install
-
+./node.msi
 #nircmd.exe (for taking screenshots from the cmd line) 
-wget https://www.nirsoft.net/utils/nircmd.zip
+curl -o nircmd.zip https://www.nirsoft.net/utils/nircmd.zip
 #Unzip and move to duo folder
-
-#curl 
-sudo apt install --yes curl jq
-# * jq
-#sudo apt get install jq
-
+unzip nircmd.zip
+#Make dicord executalle 
 sudo chmod u+x ./easyuo2discord.sh
+#Install discord.js depency
 npm install --save discord.js
